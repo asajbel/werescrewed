68d85f205b99b78799e08387e23526d9ccb733ee
Anders Sajbel <asajbel@ucsc.edu>
2013-01-11 18:54:47 -0800

    Initial Clean Project

---
+++ werescrewed-desktop/src/com/blindtigergames/werescrewed/Main.java
@@ -0,0 +1,16 @@
+package com.blindtigergames.werescrewed;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "werescrewed";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new WereScrewed(), cfg);
+	}
+}
