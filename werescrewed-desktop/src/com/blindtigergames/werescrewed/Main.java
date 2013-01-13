322a6e9d96afcf49579efc4c552193e19143a24b
Ranveeer <legendxmyth@gmail.com>
2013-01-13 14:20:07 -0800

    resolution

--- werescrewed-desktop/src/com/blindtigergames/werescrewed/Main.java
+++ werescrewed-desktop/src/com/blindtigergames/werescrewed/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "werescrewed";
 		cfg.useGL20 = true;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 1280;
+		cfg.height = 720;
 		
 		new LwjglApplication(new WereScrewed(), cfg);
 	}
