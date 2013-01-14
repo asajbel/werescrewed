7b576e790a6db622c520196f2c5189589fe0e75a
stewartbracken <stew.bracken@gmail.com>
2013-01-14 14:25:24 -0800

    FIXIN GWT launcher error

--- werescrewed-html/src/com/blindtiger/werescrewed/client/GwtLauncher.java
+++ werescrewed-html/src/com/blindtiger/werescrewed/client/GwtLauncher.java
@@ -1,6 +1,6 @@
 package com.blindtiger.werescrewed.client;
 
-import com.blindtiger.werescrewed.WereScrewed;
+import com.blindtiger.werescrewed.WereScrewedGame;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
@@ -14,6 +14,6 @@ public class GwtLauncher extends GwtApplication {
 
 	@Override
 	public ApplicationListener getApplicationListener () {
-		return new WereScrewed();
+		return new WereScrewedGame();
 	}
 }
