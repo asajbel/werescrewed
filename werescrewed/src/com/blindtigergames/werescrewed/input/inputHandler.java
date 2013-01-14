8827fb1bfaab8080117019cc4a1f2f7c88e2edd3
eboning <eboning@ucsc.edu>
2013-01-14 11:47:17 -0800

    Changed InputHandler to an interface
    There will likely be one class each for android and desktop input.

--- werescrewed/src/com/blindtigergames/werescrewed/input/inputHandler.java
+++ werescrewed/src/com/blindtigergames/werescrewed/input/inputHandler.java
@@ -3,114 +3,55 @@ package com.blindtigergames.werescrewed.input;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 
-public class inputHandler implements InputProcessor {
+public interface inputHandler extends InputProcessor {
 	public enum Player { ONE, TWO}
 	
-	public boolean isLeftPressed (Player plyr){
-	
+	public boolean isLeftPressed (Player plyr);
 		
-		return false;
+	public boolean isRightPressed (Player plyr);
 	
-	}
+	public boolean isJumpPressed (Player plyr);
 	
+	public boolean isDownPressed (Player plyr);
 	
-	public boolean isRightPressed (Player plyr){
+	public boolean screwPressed (Player plyr);
 	
-		
-		return false;
+	public boolean selectPressed (Player plyr);
 	
-	}
-	
-	public boolean isJumpPressed (Player plyr){
-	
-		
-		return false;
-	
-	}
-	
-	public boolean isDownPressed (Player plyr){
-	
-		
-		return false;
-	
-	}
-	
-	public boolean screwPressed (Player plyr){
-	
-		
-		return false;
-	
-	}
-	
-	public boolean selectPressed (Player plyr){
-	
-		
-		return false;
-	
-	}
-	
-	public boolean cancelPressed (Player plyr){
-	
-		
-		return false;
-	
-	}
+	public boolean cancelPressed (Player plyr);
+
 
 
 	@Override
-	public boolean keyDown(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean keyDown(int keycode);
 
 
 	@Override
-	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean keyUp(int keycode);
 
 
 	@Override
-	public boolean keyTyped(char character) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean keyTyped(char character);
 
 
 	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean touchDown(int screenX, int screenY, int pointer, int button);
 
 
 	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean touchUp(int screenX, int screenY, int pointer, int button);
 
 
 	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean touchDragged(int screenX, int screenY, int pointer);
 
 
 	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean mouseMoved(int screenX, int screenY);
 
 
 	@Override
-	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	public boolean scrolled(int amount);
 	
 
 }
