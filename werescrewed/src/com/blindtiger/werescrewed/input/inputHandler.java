30f77191422c65bae78adab636bc8b1060175ced
Anders Sajbel <asajbel@ucsc.edu>
2013-01-14 14:49:15 -0800

    Revert "Reverting Stewert"
    This reverts commit f156803619e37cee4bc689a198887cb22346ea41.

---
+++ werescrewed/src/com/blindtiger/werescrewed/input/inputHandler.java
@@ -0,0 +1,57 @@
+package com.blindtiger.werescrewed.input;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+
+public interface inputHandler extends InputProcessor {
+	public enum Player { ONE, TWO}
+	
+	public boolean isLeftPressed (Player plyr);
+		
+	public boolean isRightPressed (Player plyr);
+	
+	public boolean isJumpPressed (Player plyr);
+	
+	public boolean isDownPressed (Player plyr);
+	
+	public boolean screwPressed (Player plyr);
+	
+	public boolean selectPressed (Player plyr);
+	
+	public boolean cancelPressed (Player plyr);
+
+
+
+	@Override
+	public boolean keyDown(int keycode);
+
+
+	@Override
+	public boolean keyUp(int keycode);
+
+
+	@Override
+	public boolean keyTyped(char character);
+
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button);
+
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button);
+
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer);
+
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY);
+
+
+	@Override
+	public boolean scrolled(int amount);
+	
+
+}
