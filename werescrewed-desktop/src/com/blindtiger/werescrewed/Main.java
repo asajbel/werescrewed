8625ff0ab44a00dfe196811606a40e5cb7a2dd58
Anders Sajbel <asajbel@ucsc.edu>
2013-01-14 14:50:47 -0800

    Revert "Revert "Reverting Stewert""
    This reverts commit 30f77191422c65bae78adab636bc8b1060175ced.

--- werescrewed-desktop/src/com/blindtiger/werescrewed/Main.java
+++ werescrewed-desktop/src/com/blindtiger/werescrewed/Main.java
@@ -2,7 +2,6 @@ package com.blindtiger.werescrewed;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.blindtiger.werescrewed.WereScrewedGame;
 
 public class Main {
 	public static void main(String[] args) {
