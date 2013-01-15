f3f9c769eaac3f9d79aa4890c8eb31d1cf4b38a7
eboning <eboning@ucsc.edu>
2013-01-14 16:34:17 -0800

    InputHandler.java, recommitting.

---
+++ werescrewed/src/com/blindtigergames/werescrewed/input/InputHandler.java
@@ -0,0 +1,63 @@
+package com.blindtigergames.werescrewed.input;
+
+import com.badlogic.gdx.InputProcessor;
+
+public class InputHandler implements InputProcessor {
+
+	public boolean leftPressed;
+	public boolean rightPressed;
+	public boolean jumpPressed;
+	public boolean downPressed;
+	public boolean screwPressed;
+	public boolean screwingClockwise;
+	public boolean screwingCounterClockwise;
+	
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
