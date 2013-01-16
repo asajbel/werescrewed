ba97f7dd26824e7ca866604df18f012d1b5b8eca
abigpotostew <stew.bracken@gmail.com>
2013-01-15 20:41:20 -0800

    Revert "Revert "Revert "Revert "IMover updated description and overloaded move()""""
    This reverts commit 5c8b593d56182bb3819ce9a5abe59e9e879dfa37.

--- werescrewed/src/com/blindtigergames/werescrewed/entity/mover/IMover.java
+++ werescrewed/src/com/blindtigergames/werescrewed/entity/mover/IMover.java
@@ -2,15 +2,16 @@ package com.blindtigergames.werescrewed.entity.mover;
 
 import com.badlogic.gdx.physics.box2d.Body;
 
-/*******************************************
- * IMover Interface - Move a Box2D body
- * @author stew
+/*******************************************************************************
+ * IMover Interface -
+ * Move a Box2D body
  *
- *******************************************/
+ ******************************************************************************/
 public interface IMover {
 
-	/*
-	 * move() - Calculates movement then applies
+	/**
+	 * 
+	 * Calculates movement then applies
 	 *  a movement to a Box2D body.
 	 */
 	void move(Body body);
