afa69d7169bae8d9eb05de0e96cf22a5fded0b7f
Ranveeer <legendxmyth@gmail.com>
2013-01-30 18:42:59 -0800

    controller listener formating
    since it implements an interface there are javadocs already

--- werescrewed/src/com/blindtigergames/werescrewed/input/MyControllerListener.java
+++ werescrewed/src/com/blindtigergames/werescrewed/input/MyControllerListener.java
@@ -1,83 +1,97 @@
 package com.blindtigergames.werescrewed.input;
 
-
 import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.ControllerListener;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.controllers.PovDirection;
 import com.badlogic.gdx.math.Vector3;
 
-public class MyControllerListener implements ControllerListener{
+/**
+ * My controller listener class grabs input from controllers
+ * 
+ * @author Ranveer
+ * 
+ */
 
-	
-	public int indexOf(Controller controller) {
-		return Controllers.getControllers().indexOf(controller, true);
-	}
-	
-	@Override
-	public boolean accelerometerMoved( Controller arg0, int arg1, Vector3 arg2 ) {
-		// TODO Auto-generated method stub
-		return false;
+public class MyControllerListener implements ControllerListener {
+
+	/**
+	 * Function returns the what index the controller is (player 1 or player 2)
+	 * 
+	 * @author Ranveer
+	 * @param controller
+	 * @return int
+	 */
+	public int indexOf( Controller controller ) {
+		return Controllers.getControllers( ).indexOf( controller, true );
 	}
 
 	@Override
-	public boolean axisMoved( Controller arg0, int arg1, float arg2 ) {
-		//System.out.println("#" + indexOf(arg0) + ", button " + arg1 + " down");
-		//System.out.println( "axis:" + arg2 );
-		
+	public boolean axisMoved( Controller controller, int buttonIndex,
+			float axisPoint ) {
+
 		/*
-		 * for control sticks
-		 * left stick
-		 * button 0 - vertical - top is -1.0, bottom is 1.0
-		 * button 1 - horizontal - left is -1.0, right is 1.0
+		 * for control sticks left stick button 0 - vertical - top is -1.0,
+		 * bottom is 1.0 button 1 - horizontal - left is -1.0, right is 1.0
 		 * 
-		 * right stick
-		 * button 2 - vertical
-		 * button 3 - horizontal
+		 * right stick button 2 - vertical button 3 - horizontal
 		 */
 		return false;
 	}
 
 	@Override
 	public boolean buttonDown( Controller controller, int buttonIndex ) {
-		//System.out.println("#" + indexOf(controller) + ", button " + buttonIndex + " down");
+		// System.out.println("#" + indexOf(controller) + ", button " +
+		// buttonIndex + " down");
 		return false;
 	}
 
 	@Override
-	public boolean buttonUp( Controller arg0, int arg1 ) {
+	public boolean buttonUp( Controller controller, int buttonIndex ) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public void connected( Controller arg0 ) {
+	public void connected( Controller controller ) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void disconnected( Controller arg0 ) {
+	public void disconnected( Controller controller ) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public boolean povMoved( Controller arg0, int arg1, PovDirection arg2 ) {
-		//System.out.println("#" + indexOf(arg0) + ", button " + arg1 + " down");
+	public boolean povMoved( Controller controller, int buttonIndex,
+			PovDirection direction ) {
 		return false;
 	}
 
+	// Unused
 	@Override
-	public boolean xSliderMoved( Controller arg0, int arg1, boolean arg2 ) {
-		//System.out.println("#" + indexOf(arg0) + ", button " + arg1 + " down");
+	public boolean xSliderMoved( Controller controller, int sliderCode,
+			boolean value ) {
+		// TODO Auto-generated method stub
 		return false;
 	}
 
+	// Unused
 	@Override
-	public boolean ySliderMoved( Controller arg0, int arg1, boolean arg2 ) {
-		//System.out.println("#" + indexOf(arg0) + ", button " + arg1 + " down");
+	public boolean ySliderMoved( Controller controller, int sliderCode,
+			boolean value ) {
+		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
+	// Unused
+	@Override
+	public boolean accelerometerMoved( Controller controller,
+			int accelerometerCode, Vector3 value ) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
