dddbeceb81f5ca624d69463d1bd817d7027a4b37
Anders Sajbel <asajbel@ucsc.edu>
2013-02-06 16:13:53 -0800

    Reordered tiled platform

--- werescrewed/src/com/blindtigergames/werescrewed/platforms/TiledPlatform.java
+++ werescrewed/src/com/blindtigergames/werescrewed/platforms/TiledPlatform.java
@@ -25,32 +25,32 @@ import com.blindtigergames.werescrewed.util.Util;
  */
 
 public class TiledPlatform extends Platform {
-	protected float tileHeight, tileWidth;
-	protected Vector2 bodypos;
-	protected TileSet tileSet;
-
-	protected enum Shape {
-		SINGLE, VERTICAL, HORIZONTAL, RECTANGLE
-	}
-
-	protected Shape shape = Shape.SINGLE;
-
 	protected class Tile {
 		public float xOffset, yOffset;
 		public Sprite tileSprite;
-
+	
 		public Tile( ) {
 			xOffset = 0;
 			yOffset = 0;
 			sprite = null;
 		}
-
+	
 		public Tile( float offset_x, float offset_y, Sprite the_sprite ) {
 			xOffset = offset_x;
 			yOffset = offset_y;
 			tileSprite = the_sprite;
 		}
 	}
+
+	protected enum Shape {
+		SINGLE, VERTICAL, HORIZONTAL, RECTANGLE
+	}
+
+	protected float tileHeight, tileWidth;
+	protected Vector2 bodypos;
+	protected TileSet tileSet;
+
+	protected Shape shape = Shape.SINGLE;
 
 	protected Vector< Tile > tiles;
 
