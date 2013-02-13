c29cea3899c8cd9908371bdbad66a9db0b2420b9
Ranveeer <legendxmyth@gmail.com>
2013-02-12 19:45:52 -0800

    better rope code
    still needs clean up

--- werescrewed/src/com/blindtigergames/werescrewed/entity/Rope.java
+++ werescrewed/src/com/blindtigergames/werescrewed/entity/Rope.java
@@ -1,5 +1,7 @@
 package com.blindtigergames.werescrewed.entity;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
@@ -17,27 +19,31 @@ public class Rope extends Entity {
 	
 	Body tail;
 	Body link;
-
+	private ArrayList<Body> pieces;
+	
 	public Rope( String name, Vector2 pos, Texture texture, World world ) {
 		super( name, pos, texture, null, true );
 		
-
+		pieces = new ArrayList<Body>( );
+		
 		BodyDef bodyDef = new BodyDef();
 		bodyDef.type = BodyType.StaticBody;
 		bodyDef.position.set( pos );
 		bodyDef.gravityScale = .01f;
 		FixtureDef fixDef = new FixtureDef();
 		PolygonShape polygonShape = new PolygonShape();
-		polygonShape.setAsBox( .05f, 0.03f );
+		polygonShape.setAsBox( 16.0f * Util.PIXEL_TO_BOX, 16.0f * Util.PIXEL_TO_BOX );
 		fixDef.shape = polygonShape;
 		fixDef.density = 0.1f;
 		body = world.createBody( bodyDef );
 		body.createFixture( fixDef );
-		link = body;
+		pieces.add( body );
+		//link = body;
 		
-		for ( int i = 1; i <= 10; i++ ){
+		for ( int i = 0; i < 10; ++i ){
 			bodyDef = new BodyDef();
-			bodyDef.position.set( new Vector2 ( pos.x, pos.y - i * 32.0f * Util.PIXEL_TO_BOX) );
+			bodyDef.position.set( new Vector2 ( pieces.get( pieces.size( )-1 ).getWorldCenter( ).x, 
+					pieces.get( pieces.size( )-1 ).getWorldCenter( ).y  - 32.0f * Util.PIXEL_TO_BOX) );
 			bodyDef.type = BodyType.DynamicBody;
 			bodyDef.gravityScale = 0.1f;
 			polygonShape.setAsBox( 4.0f * Util.PIXEL_TO_BOX, 16.0f * Util.PIXEL_TO_BOX );
@@ -49,33 +55,42 @@ public class Rope extends Entity {
 			body = world.createBody( bodyDef );
 			body.createFixture( fixtureDef );
 			
+			
 			RevoluteJointDef revoluteJointDef = new RevoluteJointDef( );
-			revoluteJointDef.initialize( link, body, new Vector2 ( body.getWorldCenter( ).x,
+			revoluteJointDef.initialize( pieces.get( pieces.size( )-1 ), body, new Vector2 ( body.getWorldCenter( ).x,
 					body.getWorldCenter().y + 16.0f * Util.PIXEL_TO_BOX ) );
 			revoluteJointDef.enableMotor = false;
 			revoluteJointDef.collideConnected = false;
 			world.createJoint( revoluteJointDef );	
 			body.setUserData( this );
-			link = body;
+			//link = body;
+			pieces.add( body );
+
 		}
-		polygonShape.dispose( );
 		
-//		bodyDef.position.set(  new Vector2 ( pos.x, pos.y - 11 * 0.1f) );
+		//polygonShape.dispose( );
+		
+		
+//		bodyDef.position.set(  new Vector2 ( pieces.get( pieces.size( )-1 ).getWorldCenter( ).x,
+//				pieces.get( pieces.size( )-1 ).getWorldCenter().y  - 32.0f * Util.PIXEL_TO_BOX ) );
 //		FixtureDef fixtureDef = new FixtureDef();
-//		polygonShape.setAsBox( 0.03f, 0.05f );
+//		polygonShape.setAsBox(  64.0f * Util.PIXEL_TO_BOX, 16.0f * Util.PIXEL_TO_BOX );
 //		fixtureDef.shape = polygonShape;
 //		fixtureDef.isSensor = false;
-//		fixtureDef.density = 0.5f;
+//		fixtureDef.density = 10.0f;
 //		body = world.createBody( bodyDef );
 //		body.createFixture( fixtureDef );
 //		RevoluteJointDef revoluteJointDef = new RevoluteJointDef( );
-//		revoluteJointDef.initialize( link, body, body.getPosition( ) );
+//		revoluteJointDef.initialize( pieces.get( pieces.size( )-1 ), body,  new Vector2 ( body.getWorldCenter( ).x,
+//				body.getWorldCenter().y + 16.0f * Util.PIXEL_TO_BOX ) );
 //		revoluteJointDef.enableMotor = false;
 //		revoluteJointDef.enableLimit = true;
-//		revoluteJointDef.lowerAngle = 45 * MathUtils.degreesToRadians;
-//		revoluteJointDef.upperAngle = 180 * MathUtils.degreesToRadians;
 //		world.createJoint( revoluteJointDef );
+//		
+//		pieces.add(body);
 	
+		for( Body b : pieces)
+			System.out.println( b.toString( ));
 		// TODO Auto-generated constructor stub
 	
 	}
@@ -83,7 +98,8 @@ public class Rope extends Entity {
 	@Override
 	public void update( float deltatime ) {
 		if(Gdx.input.isKeyPressed( Keys.O ))
-			body.applyLinearImpulse( new Vector2(5.5f, 0.0f), body.getWorldCenter( ) );
+			pieces.get( pieces.size( )-1 ).applyLinearImpulse( new Vector2(5.5f, 0.0f),
+					pieces.get( pieces.size( )-1 ).getWorldCenter( ) );
 		
 		//System.out.println(body.toString( ));
 		
