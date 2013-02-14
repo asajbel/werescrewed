83c2588d397dede8cd12d03dcbda0675caebe30e
dfoley23 <dmfoley@ucsc.edu>
2013-02-14 14:03:21 -0800

    added particle system for puzzle entity spawn

--- werescrewed/src/com/blindtigergames/werescrewed/player/Player.java
+++ werescrewed/src/com/blindtigergames/werescrewed/player/Player.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.controllers.PovDirection;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Filter;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.JointEdge;
