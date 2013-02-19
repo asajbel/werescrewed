6e64d7ec8e6235ca68affca3296d5edeb1e0eba0
EARink <edi.ramirez@gmail.com>
2013-02-19 14:17:14 -0800

    Stomped a bug, zoom works back the way it did

--- werescrewed/src/com/blindtigergames/werescrewed/camera/Camera.java
+++ werescrewed/src/com/blindtigergames/werescrewed/camera/Camera.java
@@ -272,7 +272,7 @@ public class Camera {
 	//@SuppressWarnings( "unused" )
 	private void zoom( ) {
 		float temp = AnchorList.getInstance( ).specialDistance( ) / viewportHeight;
-		if (temp > 1f) camera.zoom = temp;
+		if (temp >= 1f) camera.zoom = temp;
 		translateBuffer.width = screenBounds.width * BUFFER_RATIO;
 		translateBuffer.height = screenBounds.height * BUFFER_RATIO;
 	}
