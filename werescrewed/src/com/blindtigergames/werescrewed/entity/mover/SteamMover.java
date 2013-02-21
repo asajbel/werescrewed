d92cdbf647d873f38d9d798f9602ff0dc7efed6e
unknown <wpacini@ucsc.edu>
2013-02-21 15:40:45 -0800

    pushing steam stuff for home

--- werescrewed/src/com/blindtigergames/werescrewed/entity/mover/SteamMover.java
+++ werescrewed/src/com/blindtigergames/werescrewed/entity/mover/SteamMover.java
@@ -1,27 +1,40 @@
 package com.blindtigergames.werescrewed.entity.mover;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.blindtigergames.werescrewed.platforms.Platform;
 import com.blindtigergames.werescrewed.screws.PuzzleScrew;
 
-public class SteamMover implements IMover{
+public class SteamMover implements IMover {
+
+	float rangeWidth;
+	float rangeHeight;
+	Vector2 localPosition;
+	int emissionDirection;
+	private boolean done = false;
+
+	public SteamMover( float height, float width, int direction ) {
+		rangeHeight = height;
+		rangeWidth = width;
+		emissionDirection = direction;
+		localPosition = new Vector2( 0f, 0f );
+	}
 
 	@Override
 	public void move( float deltaTime, Body body ) {
-		// TODO Auto-generated method stub
+		body.applyLinearImpulse( new Vector2( 0f, 1f ), body.getWorldCenter( ) );
 		
 	}
 
 	@Override
 	public void move( float deltaTime, Body body, SteeringOutput steering ) {
-		// TODO Auto-generated method stub
-		
+		move( deltaTime, body );
 	}
 
 	@Override
 	public void runPuzzleMovement( PuzzleScrew screw, float screwVal, Platform p ) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -29,5 +42,12 @@ public class SteamMover implements IMover{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
+	/**
+	 * @return true when particle is out of range
+	 */
+	public boolean atEnd( ) {
+		return done;
+	}
+
 }
