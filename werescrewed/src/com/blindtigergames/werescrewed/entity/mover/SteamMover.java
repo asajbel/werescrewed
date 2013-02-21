2c452d02530870191c8025157cdfb2155f2fa9e5
unknown <wpacini@ucsc.edu>
2013-02-21 15:34:12 -0800

    still working on particles

---
+++ werescrewed/src/com/blindtigergames/werescrewed/entity/mover/SteamMover.java
@@ -0,0 +1,33 @@
+package com.blindtigergames.werescrewed.entity.mover;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.blindtigergames.werescrewed.platforms.Platform;
+import com.blindtigergames.werescrewed.screws.PuzzleScrew;
+
+public class SteamMover implements IMover{
+
+	@Override
+	public void move( float deltaTime, Body body ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move( float deltaTime, Body body, SteeringOutput steering ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void runPuzzleMovement( PuzzleScrew screw, float screwVal, Platform p ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PuzzleType getMoverType( ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
