c245b292c99498e0b0f9272cb4b6748df1c58a8c
Bryan Pacini <wpacini@ucsc.com>
2013-02-20 16:15:49 -0800

    getting to pull

--- werescrewed/src/com/blindtigergames/werescrewed/particles/ParticleSystem.java
+++ werescrewed/src/com/blindtigergames/werescrewed/particles/ParticleSystem.java
@@ -9,7 +9,13 @@ public class ParticleSystem {
 
 	private ArrayList< Particle > particles;
 
-	public ParticleSystem( Entity baseEntity, IMover mover, float lifeSpan ) {
+	/**
+	 * Creates Particle System with one initial particle
+	 * @param baseEntity Entity
+	 * @param lifeSpan float
+	 * @param mover mover
+	 */
+	public ParticleSystem( Entity baseEntity, float lifeSpan, IMover mover ) {
 		particles = new ArrayList< Particle >( );
 		Particle p = new Particle( baseEntity, lifeSpan, mover );
 		particles.add( p );
@@ -23,15 +29,21 @@ public class ParticleSystem {
 			}
 		}
 	}
-
-	public void addParticle( Entity e, float lifeSpan, IMover mover ) {
-		particles.add( new Particle( e, lifeSpan, mover ) );
+	
+	/**
+	 * adds new particle to the engine with the following components
+	 * @param entity Entity
+	 * @param lifeSpan float
+	 * @param mover mover
+	 */
+	public void addParticle( Entity entity, float lifeSpan, IMover mover ) {
+		particles.add( new Particle( entity, lifeSpan, mover ) );
 	}
 
-	public void duplicateParticle( Entity e, float lifeSpan, IMover mover,
+	/*public void duplicateParticle( Entity e, float lifeSpan, IMover mover,
 			int duplications ) {
 		for ( int i = 0; i < duplications; i++ ) {
 			particles.add( new Particle( e, lifeSpan, mover ) );
 		}
-	}
+	}*/
 }
