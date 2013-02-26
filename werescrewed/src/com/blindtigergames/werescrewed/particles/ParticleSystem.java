77d7acbbd92670edb0a0262045971833ac7b3fb0
Bryan Pacini <wpacini@ucsc.com>
2013-02-26 12:48:10 -0800

    fixing merge issues

--- werescrewed/src/com/blindtigergames/werescrewed/particles/ParticleSystem.java
+++ werescrewed/src/com/blindtigergames/werescrewed/particles/ParticleSystem.java
@@ -9,7 +9,6 @@ public class ParticleSystem {
 
 	private ArrayList< Particle > particles;
 
-<<<<<<< HEAD
 	/**
 	 * Creates Particle System with one initial particle
 	 * @param baseEntity Entity
@@ -17,17 +16,6 @@ public class ParticleSystem {
 	 * @param mover mover
 	 */
 	public ParticleSystem( Entity baseEntity, float lifeSpan, IMover mover ) {
-=======
-	
-	/**
-	 * Builds new particle engine with initial particle
-	 * 
-	 * @param baseEntity Entity
-	 * @param mover IMover
-	 * @param lifeSpan float
-	 */
-	public ParticleSystem( Entity baseEntity, IMover mover, float lifeSpan ) {
->>>>>>> 412068974b2f3236d2916a0afef9766ead8b03b3
 		particles = new ArrayList< Particle >( );
 		Particle p = new Particle( baseEntity, lifeSpan, mover );
 		particles.add( p );
@@ -48,8 +36,8 @@ public class ParticleSystem {
 	}
 	
 	/**
-<<<<<<< HEAD
 	 * adds new particle to the engine with the following components
+	 * 
 	 * @param entity Entity
 	 * @param lifeSpan float
 	 * @param mover mover
@@ -58,17 +46,6 @@ public class ParticleSystem {
 		particles.add( new Particle( entity, lifeSpan, mover ) );
 	}
 
-	/*public void duplicateParticle( Entity e, float lifeSpan, IMover mover,
-=======
-	 * adds a new particle to the system
-	 * 
-	 * @param e Entity
-	 * @param lifeSpan float
-	 * @param mover IMover
-	 */ 
-	public void addParticle( Entity e, float lifeSpan, IMover mover ) {
-		particles.add( new Particle( e, lifeSpan, mover ) );
-	}
 	
 	/**
 	 * don't use this right now
@@ -79,15 +56,10 @@ public class ParticleSystem {
 	 * @param duplications int
 	 */
 	public void duplicateParticle( Entity e, float lifeSpan, IMover mover,
->>>>>>> 412068974b2f3236d2916a0afef9766ead8b03b3
 			int duplications ) {
 		for ( int i = 0; i < duplications; i++ ) {
 			particles.add( new Particle( e, lifeSpan, mover ) );
 		}
-<<<<<<< HEAD
-	}*/
-=======
 	}
 	
->>>>>>> 412068974b2f3236d2916a0afef9766ead8b03b3
 }
