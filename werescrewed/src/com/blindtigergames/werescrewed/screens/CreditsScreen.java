845b8f42247a83f0481401f1c12bdb0593ea4783
Victor Nguyen <vitnguye@ucsc.edu>
2013-03-11 17:09:43 -0700

    Added a new font
    It's called Screwball and I found it here
    http://www.abstractfonts.com/search/?q=screw

--- werescrewed/src/com/blindtigergames/werescrewed/screens/CreditsScreen.java
+++ werescrewed/src/com/blindtigergames/werescrewed/screens/CreditsScreen.java
@@ -29,7 +29,7 @@ class CreditsScreen implements com.badlogic.gdx.Screen {
 	public CreditsScreen( ) {
 		batch = new SpriteBatch( );
 		font = new BitmapFont( );
-		fancyFont = WereScrewedGame.manager.getFont( "ornatique" );
+		fancyFont = WereScrewedGame.manager.getFont( "Screwball" );
 		logo =  WereScrewedGame.manager.get( WereScrewedGame.dirHandle
 				 + "/common/title_background.png", Texture.class );
 		lineHeight = Math.round( 2.5f * font.getCapHeight( ) );
