6c8f8afded9e02e4188a5287332120348c509435
Bryan Pacini <wpacini@ucsc.com>
2013-03-16 17:37:06 -0700

    changed sensors

--- werescrewed/src/com/blindtigergames/werescrewed/collisionManager/MyContactListener.java
+++ werescrewed/src/com/blindtigergames/werescrewed/collisionManager/MyContactListener.java
@@ -273,7 +273,6 @@ public class MyContactListener implements ContactListener {
 							}
 							break;
 						case STEAM:
-							Gdx.app.log( "", "Big FLoppy Donkey Dick" );
 							player.setSteamCollide( false );
 							break;
 						case EVENTTRIGGER:
