a6a5d2189aefb7de2b31cb09bd53a46e5ebf6c5b
Kevin <kevin@kevin-Gateway-Viper-C.(none)>
2013-04-12 13:43:36 -0700

    More SoundManager work. Added a new sound type called Noise that is technically a sound effect but is treated as a music object.

--- werescrewed/src/com/blindtigergames/werescrewed/sound/SoundManager.java
+++ werescrewed/src/com/blindtigergames/werescrewed/sound/SoundManager.java
@@ -1,120 +1,199 @@
 package com.blindtigergames.werescrewed.sound;
 
 import java.util.EnumMap;
+import java.util.HashMap;
 
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.utils.Disposable;
 import com.blindtigergames.werescrewed.WereScrewedGame;
 import com.blindtigergames.werescrewed.camera.Camera;
+import com.blindtigergames.werescrewed.entity.I_Updateable;
 
-public class SoundManager {
-	public enum SOUND_TYPE{
+public class SoundManager implements I_Updateable {
+	public enum SoundType{
+		/* Background music, as you can expect. */
 		MUSIC,
+		/* Sound effects that are expected to play only a single instance at least semi-constantly, 
+		 * and therefore are treated as music internally.*/
+		NOISE,
+		/* Regular sound effects should be expected to play more than one instance at a time.
+		 * Will probably be used for collisions.*/
 		SFX,
+		/* Still not sure if speech should be implemented as music or sound. We'll have to see
+		 * how far we can break up each sound file.*/
 		SPEECH
 	}
 	
-	public EnumMap<SOUND_TYPE, Float> volume;
+	public EnumMap<SoundType, Float> volume;
+	public EnumMap<SoundType, HashMap<String, SoundRef>> refs;
 	
 	protected Camera camera;
 	
 	public SoundManager( ) {
-		volume = new EnumMap<SOUND_TYPE, Float>(SOUND_TYPE.class);
-		for (SOUND_TYPE type: SOUND_TYPE.values( )){
+		volume = new EnumMap<SoundType, Float>(SoundType.class);
+		refs = new EnumMap<SoundType, HashMap<String, SoundRef>>(SoundType.class);
+		for (SoundType type: SoundType.values( )){
 			volume.put( type, 1.0f );
+			refs.put( type, new HashMap<String, SoundRef>() );
 		}
 		camera = null;
 	}
 
-	public class SoundRef{
-		SoundMod amplitude;
-		SoundMod frequency;
-		SoundMod pan;
+	public void update( float deltaTime ){
+	}
+	
+	public SoundRef getSound(SoundType t, String n){
+		return new SoundRef(this, t, n);
+	}
+	
+	public class SoundRef implements I_Updateable, Disposable {
+		public float volume;
+		public float frequency;
+		public float pan;
 		SoundManager manager;
 		
 		Sound sound;
 		Music music;
+		boolean looping;
 		
-		SOUND_TYPE type;
+		SoundType type;
 		
-		protected SoundRef(SoundManager m, SOUND_TYPE t, String name){
+		protected SoundRef(SoundManager m, SoundType t, String name){
 			manager = m;
 			type = t;
-			amplitude = new SoundMod(this);
-			frequency = new SoundMod(this);
-			pan = new SoundMod(this);
-			pan.flat = 0f;
+			volume = 1.0f;
+			frequency = 1.0f;
+			pan = 0.0f;
+			looping = false;
 			sound = null; music = null;
 			switch (type){
+				case SPEECH:
+				case NOISE:
 				case MUSIC:
 					music = WereScrewedGame.manager.get( name, Music.class );
+					break;
 				default:
 					sound = WereScrewedGame.manager.get( name, Sound.class );
 			}
 		}
 		
-		public void play(){
+		public long play(){
 			if (music != null){
 				music.setVolume( getVolume() );
 				music.play( );
+				return -1;
 			} else {
-				sound.play( getVolume(), getFrequency(), getPan() );
+				return sound.play( getVolume(), getFrequency(), getPan() );		
 			}
 		}
 		
-		public void loop(boolean v){
+		public long loop(){
 			if (music != null){
-				music.setLooping( v );
-				if (v) {play();}
-			} else {
-				sound.loop( getVolume(), getFrequency(), getPan() );
+				music.setLooping( true );
 			}
+			looping = true;
+			return this.play();
 		}
 				
 		public float getVolume(){
-			float vol = manager.volume.get( type ) * amplitude.calculate( );
-			return Math.min( 1.0f, Math.max( vol, 0f ) );
+			float vol = manager.volume.get( type ) * volume;
+			return Math.min( 2.0f, Math.max( vol, 0f ) );
 		}
 		
 		public float getFrequency(){
-			return Math.min( 2f, Math.max( frequency.calculate(), 0.01f));
+			return Math.min( 2f, Math.max( frequency, 0.01f));
 		}
 		
 		public float getPan(){
-			return Math.min( 1f, Math.max( pan.calculate(), -1f));
+			return Math.min( 1f, Math.max( pan, -1f));
 		}
 		
-		public class SoundMod{
-			SoundRef ref;
-			float dX; //Horizontal distance from camera.
-			float dY; //Vertical distance from camera.
-			float c;  //Collision force
-			float cV; //Camera velocity
-			float wV; //World velocity
-			float s;  //Screwing speed
-			float aV; //Angular velocity
-			float flat; //Constant
-			
-			
-			SoundMod(SoundRef nRef){
-				ref = nRef;
-				dX = 0f;
-				dY = 0f;
-				c = 0f;
-				cV = 0f;
-				wV = 0f;
-				s = 0f;
-				aV = 0f;
-				flat = 1f;
+		public boolean isMusic(){
+			return music != null;
+		}
+
+		@Override
+		public void update( float dT ) {
+			if (music != null){
+				music.setVolume( getVolume() );
 			}
-			
-			public float calculate(){
-				return flat;
-			}
-			
-			public float calculate(float DX, float DY, float C, float CV, float WV, float S, float AV){
-				return (DX * dX) + (DY * dY) + (C * c) + (CV * cV) + (WV * wV) + (S * s) + (AV * aV) + flat;
-			}
+		}
+
+		public long play( float v, float f, float p ) {			
+			volume = v;
+			frequency = f;
+			pan = p;
+			return play();
+		}
+
+		public long loop( float v) {
+			volume = v;
+			return loop();
+		}
+
+		public long loop( float v, float f, float p ) {
+			volume = v;
+			frequency = f;
+			pan = p;
+			return 0;
+		}
+
+		public void stop( ) {
+			sound.stop( );
+			music.stop( );
+		}
+
+		public void dispose( ) {
+			sound.dispose( );
+			music.dispose( );
+		}
+
+		public void stop( long soundId ) {
+			sound.stop(soundId);
+		}
+
+		public void setLooping(boolean l) {
+			music.setLooping( l );
+			looping = l;
+		}
+
+		public void setPitch( long soundId, float pitch ) {
+			sound.setPitch( soundId, pitch );
+		}
+
+		public void setVolume( long soundId, float volume ) {
+			sound.setPan( soundId, soundId, volume );
+		}
+
+		public void setPan( long soundId, float pan, float volume ) {
+			sound.setPan( soundId, pan, volume );
+		}
+
+		public void setPriority( long soundId, int priority ) {
+			sound.setPriority(soundId, priority);
+		}
+
+		public void pause( ) {
+			music.pause( );
+		}
+
+		public boolean isPlaying( ) {
+			if (isMusic()){return music.isPlaying( );}
+			return false;
+		}
+
+		public boolean isLooping( ) {
+			return looping;
+		}
+
+		public void setVolume( float volume ) {
+			this.volume = volume;
+		}
+
+		public float getPosition( ) {
+			if (isMusic()){return music.getPosition( );}
+			return 0f;
 		}
 	}
 }
