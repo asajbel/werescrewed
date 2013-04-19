49ff187af44d27e45cb9519dd35d072bd447eef5
dfoley23 <screwed@marle.soe.ucsc.edu>
2013-04-19 12:03:25 -0700

    worked on optimization the macro skeleotns will always move even if inactive anything not macro will not draw or move but the skeletons arent being set to inactive

--- werescrewed/src/com/blindtigergames/werescrewed/level/Level.java
+++ werescrewed/src/com/blindtigergames/werescrewed/level/Level.java
@@ -143,7 +143,7 @@ public class Level {
 			player2.draw( batch, deltaTime );
 		// draw all foreground entity sprites after everything
 		for ( Entity e : entityFGList ) {
-			//if ( e.isActive( ) ) 
+			if ( e.isActive( ) ) 
 			{
 				e.drawFGDecals( batch );
 			}
@@ -153,7 +153,7 @@ public class Level {
 			if ( skel.fgSprite != null && skel.fgSprite.getAlpha( ) != 0 ) {
 				skel.fgSprite.draw( batch );
 			}
-			//if ( !skel.isActive( ) ) {
+			//if ( skel.isActive( ) ) {
 				skel.drawFGDecals( batch );
 			//}
 		}
