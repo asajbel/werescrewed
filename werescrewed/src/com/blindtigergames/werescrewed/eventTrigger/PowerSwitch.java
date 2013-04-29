b5fe1c387f831816d5d25bac6e61b4334668a691
abigpotostew <stew.bracken@gmail.com>
2013-04-29 15:14:08 -0700

    Power switches in, they look like crap with their rotations

--- werescrewed/src/com/blindtigergames/werescrewed/eventTrigger/PowerSwitch.java
+++ werescrewed/src/com/blindtigergames/werescrewed/eventTrigger/PowerSwitch.java
@@ -2,23 +2,24 @@ package com.blindtigergames.werescrewed.eventTrigger;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.blindtigergames.werescrewed.WereScrewedGame;
 import com.blindtigergames.werescrewed.entity.EntityType;
+import com.blindtigergames.werescrewed.entity.Sprite;
 import com.blindtigergames.werescrewed.graphics.SpriteBatch;
+import com.blindtigergames.werescrewed.graphics.TextureAtlas;
 import com.blindtigergames.werescrewed.util.Util;
 
 
 public class PowerSwitch extends EventTrigger{
 
 	private boolean state = false;
-	private Texture onTex =  WereScrewedGame.manager.get(
-			WereScrewedGame.dirHandle + "/common/powerswitches/on.png" );
-	private Texture offTex = WereScrewedGame.manager.get( 
-			WereScrewedGame.dirHandle + "/common/powerswitches/off.png" );
+	//private static Texture onTex =  WereScrewedGame.manager.get(
+	//		WereScrewedGame.dirHandle + "/common/powerswitches/on.png" );
+	//private static Texture offTex = WereScrewedGame.manager.get( 
+	//		WereScrewedGame.dirHandle + "/common/powerswitches/off.png" );
 	
 	private Sprite onState, offState;
 	/**
@@ -32,11 +33,12 @@ public class PowerSwitch extends EventTrigger{
 		super( name, world );
 		contructRectangleBody(64, 64, position);
 		entityType = EntityType.POWERSWITCH;
-		
-		onState = new Sprite(onTex);
-		offState = new Sprite(offTex);
-		
-		
+		TextureAtlas commonTextureAtlas = WereScrewedGame.manager.getAtlas( "common-textures" );
+		onState = commonTextureAtlas.createSprite( "steam_engine_lever_on" );//new Sprite(onTex);
+		offState = commonTextureAtlas.createSprite( "steam_engine_lever_off" );//new Sprite(offTex);
+		//onState.setOrigin( onState.getWidth( )/2, 32 );
+		//offState.setOrigin( offState.getWidth( )/2, 32 );
+		addBGDecal( onState );
 	}
 	
 	public void doAction(){
@@ -64,25 +66,25 @@ public class PowerSwitch extends EventTrigger{
 	
 	@Override
 	public void draw(SpriteBatch batch, float deltaTime){
-		float xpos =  body.getPosition( ).x - (32f * Util.PIXEL_TO_BOX);
-		float ypos =  body.getPosition( ).y - (32f * Util.PIXEL_TO_BOX);
+		//float xpos =  body.getPosition( ).x - (32f * Util.PIXEL_TO_BOX);
+		//float ypos =  body.getPosition( ).y - (32f * Util.PIXEL_TO_BOX);
 		
-
-		if(state){
-			onState.setOrigin( 32f, 32f);
-			onState.setPosition( xpos * Util.BOX_TO_PIXEL, ypos * Util.BOX_TO_PIXEL);
-			onState.setRotation(  MathUtils.radiansToDegrees
-					* body.getAngle( ) );
-			onState.draw( batch );
-		}else{
-			offState.setOrigin( 32f, 32f);
-			offState.setPosition( xpos * Util.BOX_TO_PIXEL, ypos * Util.BOX_TO_PIXEL);
-			offState.setRotation(  MathUtils.radiansToDegrees
-					* body.getAngle( ) );
-			offState.draw( batch );
+		Sprite currentSprite = (state)?onState:offState;
+		if(currentSprite.equals(bgDecals.get( 0 ))){
+			bgDecals.clear( );
+			bgDecalAngles.clear( );
+			bgDecalOffsets.clear( );
+			addBGDecal( currentSprite );
 		}
+		updateDecals( deltaTime );
+
+		super.draw( batch, deltaTime );
 		
-	}
+		//currentSprite.setPosition( xpos * Util.BOX_TO_PIXEL, ypos * Util.BOX_TO_PIXEL);
+		//currentSprite.setRotation(  MathUtils.radiansToDegrees
+		//			* body.getAngle( ) );
+		
+	}	
 	
 	public boolean isTurnedOn(){
 		return state;
