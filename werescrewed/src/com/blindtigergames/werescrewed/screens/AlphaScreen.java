d746667edefe29e3ecd75d24228d32d749a07d2a
unknown <wpacini@ucsc.edu>
2013-05-03 13:53:31 -0700

    sound null pointer error  addressed

--- werescrewed/src/com/blindtigergames/werescrewed/screens/AlphaScreen.java
+++ werescrewed/src/com/blindtigergames/werescrewed/screens/AlphaScreen.java
@@ -140,7 +140,7 @@ public class AlphaScreen extends Screen {
 		// right arm: 2600f, 6000f >>>> side
 		//left side hand <- -2224, 3008
 		
-		Vector2 spawnPos = new Vector2( -1582f, 6150f );
+		Vector2 spawnPos = new Vector2( 512f, 256f );
 
 		if ( level.player1 == null ) {
 			level.player1 = new PlayerBuilder( ).world( level.world )
