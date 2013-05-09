e96e0ab1769316da12f9a1e973a0717929b8504c
abigpotostew <stew.bracken@gmail.com>
2013-05-08 22:18:14 -0700

    Removed a huge amount of old animations.

--- werescrewed/src/com/blindtigergames/werescrewed/screens/AlphaScreen.java
+++ werescrewed/src/com/blindtigergames/werescrewed/screens/AlphaScreen.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
