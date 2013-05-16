f71ce4b182c76c2ff2d4e7e6b98eb45b794f1c64
Anders Sajbel <asajbel@ucsc.edu>
2013-05-16 14:33:15 -0700

    Added background to level select

--- werescrewed/src/com/blindtigergames/werescrewed/screens/LevelSelectScreen.java
+++ werescrewed/src/com/blindtigergames/werescrewed/screens/LevelSelectScreen.java
@@ -6,9 +6,12 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.blindtigergames.werescrewed.WereScrewedGame;
+import com.blindtigergames.werescrewed.entity.Falling;
 import com.blindtigergames.werescrewed.entity.Sprite;
 import com.blindtigergames.werescrewed.entity.animator.SingleSpinemator;
+import com.blindtigergames.werescrewed.graphics.TextureAtlas;
 import com.blindtigergames.werescrewed.gui.Label;
 import com.blindtigergames.werescrewed.gui.TextButton;
 
@@ -31,6 +34,12 @@ public class LevelSelectScreen extends Screen {
 	private TextButton dragonButton = null;
 	private SingleSpinemator man = null;
 	private SingleSpinemator lady = null;
+	private Array< Falling > debris = null;
+	private Array< Falling > gears = null;
+	private int width, height;
+	private float time;
+	private float manDir = 1;
+	private float ladyDir = -1;
 
 	public LevelSelectScreen( ) {
 		super( );
@@ -48,6 +57,13 @@ public class LevelSelectScreen extends Screen {
 		screenLabel = new Label( "Level Select", fancyFont );
 		man = new SingleSpinemator( "red_male_atlas", "male", "fall_idle" );
 		lady = new SingleSpinemator( "red_female_atlas", "female", "fall_idle" );
+		gears = new Array< Falling >( );
+		debris = new Array< Falling >( );
+		TextureAtlas gearsAtlas = WereScrewedGame.manager.getAtlas( "gears" );
+		TextureAtlas common = WereScrewedGame.manager
+				.getAtlas( "common-textures" );
+		for ( int i = 0; i < 5; i++ )
+			createDebris( gearsAtlas, common );
 		loadButtons( );
 	}
 
@@ -71,12 +87,17 @@ public class LevelSelectScreen extends Screen {
 	public void render( float delta ) {
 		super.render( delta );
 
-		Gdx.gl.glClearColor( 0.0f, 0.0f, 0.0f, 1f );
+		Gdx.gl.glClearColor( 0.4f, 0.6f, 1.0f, 1f );
 		Gdx.gl.glClear( GL10.GL_COLOR_BUFFER_BIT );
 
-		man.update( delta );
-		lady.update( delta );
-		batch.begin( );
+		moveCharacters( delta );
+		updateDebris( );
+		batch.begin( );for ( Falling g : gears ) {
+			g.sprite.draw( batch );
+		}
+		for ( Falling d : debris ) {
+			d.sprite.draw( batch );
+		}
 		menuBG.draw( batch );
 		// screenLabel.draw( batch );
 		physicsButton.draw( batch, camera );
@@ -89,15 +110,17 @@ public class LevelSelectScreen extends Screen {
 
 		if ( !finish )
 			setAlpha( -0.02f );
-		transition.draw( batch, alpha );
 		man.draw( batch );
 		lady.draw( batch );
+		transition.draw( batch, alpha );
 		batch.end( );
 
 	}
 
 	@Override
 	public void resize( int width, int height ) {
+		this.width = width;
+		this.height = height;
 
 		camera = new OrthographicCamera( );
 		camera.setToOrtho( false, width, height );
@@ -109,7 +132,8 @@ public class LevelSelectScreen extends Screen {
 		@SuppressWarnings( "unused" )
 		float scaleY = height / 720f;
 
-		transition.setPosition( 0, 0 );
+		transition.setPosition( width/2 - transition.getWidth( )/2, height/2 - transition.getHeight( )/2 );
+		transition.setScale( width/transition.getWidth( ), height/transition.getHeight( ) );
 		// menuBG.setScale( width / menuBG.getWidth( ), width / menuBG.getWidth(
 		// ) );
 		menuBG.setPosition( 0, height / 2 - menuBG.getHeight( ) / 2 );
@@ -134,7 +158,13 @@ public class LevelSelectScreen extends Screen {
 		backButton.setY( centerY + lineHeight * -2 );
 
 		man.setPosition( width / 2 - 50, height / 2 + 50 );
-		lady.setPosition( width / 2 + 50, height / 2 - 50 );
+		lady.setPosition( width / 2 + 200, height / 2 - 200 );
+		for ( Falling g : gears ) {
+			resizeGears( g );
+		}
+		for ( Falling d : debris ) {
+			resizeDebris( d );
+		}
 
 	}
 
@@ -176,5 +206,115 @@ public class LevelSelectScreen extends Screen {
 		Buttons.add( backButton );
 
 	}
+	
+	private void updateDebris( ) {
+		for ( Falling g : gears ) {
+			float y = g.sprite.getY( );
+			if ( y < -g.sprite.getHeight( ) ) {
+				resetGears( g );
+				continue;
+			}
+			g.sprite.setY( y - g.fallSpeed );
+			g.sprite.rotate( g.rotateSpeed );
+		}
+		for ( Falling d : debris ) {
+			float y = d.sprite.getY( );
+			if ( y < -d.sprite.getHeight( ) ) {
+				resetDebris( d );
+				continue;
+			}
+			d.sprite.setY( y - d.fallSpeed );
+			d.sprite.rotate( d.rotateSpeed );
+		}
+	}
 
+	private void createDebris( TextureAtlas gearAtlas, TextureAtlas common ) {
+		for ( int i = 1; i <= 5; i++ ) {
+			Sprite s = gearAtlas.createSprite( "gear" + i );
+			s.setOrigin( s.getWidth( ) / 2, s.getHeight( ) / 2 );
+			Falling temp = new Falling( s );
+			gears.add( temp );
+		}
+		addCommon( common, "hex_screw" );
+		addCommon( common, "pipeLR" );
+		addCommon( common, "pipeUR" );
+		addCommon( common, "flat_head_circular2" );
+		addCommon( common, "power_screw" );
+		addCommon( common, "pipeEndL" );
+		addCommon( common, "pipeEndD" );
+		addCommon( common, "flat_head_circular" );
+		addCommon( common, "pipeEndU" );
+		addCommon( common, "switch_on" );
+		addCommon( common, "switch_off" );
+		addCommon( common, "pipeEndR" );
+		addCommon( common, "pipeDL" );
+		addCommon( common, "pipeUD" );
+		addCommon( common, "pipeUL" );
+		addCommon( common, "pipeDR" );
+		for ( int i = 0; i < 5; i++ ) {
+			addCommon( common, "chainlink" );
+			addCommon( common, "rivet1" );
+			addCommon( common, "rivet2" );
+			addCommon( common, "rivet3" );
+			addCommon( common, "rivet4" );
+		}
+	}
+
+	private void addCommon( TextureAtlas common, String name ) {
+		Sprite s = common.createSprite( name );
+		s.setOrigin( s.getWidth( ) / 2, s.getHeight( ) / 2 );
+		Falling temp = new Falling( s );
+		debris.add( temp );
+	}
+
+	private void resetGears( Falling d ) {
+		resetDebris( d );
+		d.sprite.setScale( 0.1f + ( float ) Math.random( ) * 0.4f );
+	}
+
+	private void resetDebris( Falling d ) {
+		double newX = width * Math.random( );
+		double newY = height + Math.random( ) * height / 2;
+		d.sprite.setPosition( ( float ) newX, ( float ) newY );
+		d.fallSpeed = ( float ) ( 0.5f + Math.random( ) * 1.5f );
+		d.rotateSpeed = ( float ) ( Math.random( ) * 0.7 );
+		d.sprite.setScale( 0.3f + ( float ) Math.random( ) * 0.7f );
+	}
+
+	private void resizeGears( Falling d ) {
+		resizeDebris( d );
+		d.sprite.setScale( 0.1f + ( float ) Math.random( ) * 0.4f );
+	}
+
+	private void resizeDebris( Falling d ) {
+		resetDebris( d );
+		double newX = width * Math.random( );
+		double newY = Math.random( ) * height;
+		d.sprite.setPosition( ( float ) newX, ( float ) newY );
+	}
+
+	private void moveCharacters( float delta ) {
+		time += delta / 3;
+		man.update( delta / 2 );
+		lady.update( delta / 2 );
+		float mx = man.getX( ) - ( float ) Math.cos( time - delta ) * 0.25f; 
+		float my = man.getY( ) - ( float ) Math.sin( time ) * 0.6f;
+		mx = mx + manDir * 0.5f;
+		if ( mx > width * 5 / 6 )
+			manDir -= 0.05;
+		if ( mx < menuBG.getWidth( ) * 1.5)
+			manDir += 0.05;
+		man.setPosition( mx, my );
+		float fx = lady.getX( ) + ( float ) ( Math.cos( time + delta ) * 0.25f );
+		float fy = lady.getY( ) + ( float ) Math.sin( time + delta ) * 1.1f;
+		fx = fx + ladyDir * 0.5f;
+		if ( fx > width * 5 / 6 ) {
+			ladyDir -= 0.15;
+		}
+		if ( fx < menuBG.getWidth( ) * 1.5 ) {
+			ladyDir += 0.15;
+		}
+		lady.setPosition( fx, fy );
+	}
 }
+
