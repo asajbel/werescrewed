0065102912252c085a02ae18f58576582e75dea9
abigpotostew <stew.bracken@gmail.com>
2013-06-11 14:04:33 -0700

    DEBUG OUT.

--- werescrewed-desktop/src/com/blindtigergames/werescrewed/Main.java
+++ werescrewed-desktop/src/com/blindtigergames/werescrewed/Main.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
-	private static boolean debug = true;
+	private static boolean debug = false ;
 
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
