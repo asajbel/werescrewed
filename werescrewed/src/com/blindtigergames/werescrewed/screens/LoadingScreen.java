9146e9adc7e424328585f91f5396e4455aa4617e
Anders Sajbel <asajbel@ucsc.edu>
2013-06-13 16:18:22 -0700

    Slides fixed up and alphaOut added

--- werescrewed/src/com/blindtigergames/werescrewed/screens/LoadingScreen.java
+++ werescrewed/src/com/blindtigergames/werescrewed/screens/LoadingScreen.java
@@ -155,6 +155,10 @@ public class LoadingScreen extends Screen {
 					storyBoardArray.add( WereScrewedGame.manager.get(
 							"data/common/slides/slide6_dragon.png", Texture.class ) );
 				}
+				
+				for (Texture t : storyBoardArray) {
+					t.setFilter( Texture.TextureFilter.Linear, Texture.TextureFilter.Linear ); 
+				}
 							
 				pressStart = new Label( "Press Start To Volunteer!",
 							WereScrewedGame.manager.getFont( "longdon" ) );
